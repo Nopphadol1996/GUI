b44596cb9fd8e0c9f21cc4f9bff0c66b570e9ccd
Nopphadol Kanklaew <89741372+Nopphadol1996@users.noreply.github.com>
2024-10-06 11:12:45 +0700

    Basic2

---
+++ Basic2.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+root = Tk()
+
+#ตั้งค่าหน้าจอ
+root.title('โปรแกรมของฉัน')
+root.iconbitmap("icons/logo.ico")
+root.geometry('300x300+500+200')
+root.resizable(0,0)
+root.config(bg='pink')
+
+
+root.mainloop()
