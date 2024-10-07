e29b0b2ad3f3d755999953b4a2d26eaf6bbda342
Nopphadol Kanklaew <89741372+Nopphadol1996@users.noreply.github.com>
2024-10-07 18:09:59 +0700

    Basic4

---
+++ Basic4.py
@@ -0,0 +1,27 @@
+from tkinter import *
+
+root = Tk()
+
+#ตั้งค่าหน้าจอ
+root.title('โปรแกรมของฉัน')
+root.iconbitmap("icons/logo.ico")
+root.geometry('300x300+500+200')
+root.resizable(0,0)
+root.config(bg='pink')
+
+#แสดงข้อความ
+
+label1=Label(root,text='สวัสดี',font=('Angsana New',30,'bold'),bg='red',fg='white')
+label1.pack(fill='x',padx=(5,10),pady=(5,10))
+
+label2=Label(root,text='Python',font=('Angsana New',30,'bold'),bg='blue',fg='white')
+label2.pack(pady=5, ipadx=50, ipady=10) # ipadx กำหนดระยะภายใน
+
+label3=Label(root,text='Tkinter',font=('Angsana New',30,'bold'),bg='green',fg='white')
+label3.pack(fill=BOTH, expand=True)
+
+label4=Label(root,text='GUI',font=('Angsana New',30,'bold'),bg='black',fg='white')
+label4.pack(fill=BOTH, expand=True,ipadx=10, ipady=10)
+
+
+root.mainloop()
